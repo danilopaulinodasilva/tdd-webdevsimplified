e44367d2f037ccffd253b60183630637328de8e1
Danilo Paulino da Silva <danilopaulinodasilva@gmail.com>
2020-09-19 09:00:05 -0300

    cloneArray and substract test

---
+++ substract.test.js
@@ -0,0 +1,8 @@
+const substract = require('./substract');
+
+test('properly substracing two numbers', () => {
+    expect(
+        substract(1,2)
+        .toBe(-1)
+    )
+});
--- sum.test.js
+++ sum.test.js
@@ -2,5 +2,7 @@ const { exec } = require('child_process');
 const sum = require('./sum');
 
 test('properly adds two numbers', () => {
-    expect(sum(1,2)).toBe(3)
+    expect(
+        sum(1,2))
+        .toBe(3)
 })
